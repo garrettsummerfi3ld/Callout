94e7ae911f2b10110b330e8630707bfde3130615
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-04-07 23:23:50 -0500

    Starting basic groundwork for application

--- app/src/main/java/io/github/garrettsummerfi3ld/callout/MainActivity.java
+++ app/src/main/java/io/github/garrettsummerfi3ld/callout/MainActivity.java
@@ -2,6 +2,9 @@ package io.github.garrettsummerfi3ld.callout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Local vars
+    private static final int CALLOUT_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    private final String calloutMessage = getString(R.string.callout_message_debug_test);
+
+    // Triggered by pressing the callout button in the main activity
+    public void calloutMessage(View view) {
+        // Confirmation toast
+        Toast.makeText(this, "[DEBUG] Calling out to your contacts!", Toast.LENGTH_LONG).show();
+        Log.i("calloutMessage","Triggered toast message");
     }
 }
