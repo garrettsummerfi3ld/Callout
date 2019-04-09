05caa90fd54d84ee016711f01d4bc5c4964d87f2
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-04-08 22:03:04 -0500

    Fixed a run-time error with the app crashing on launch
    This was caused by a a resource value not being properly cast as a string, thus breaking the whole app

--- app/src/main/java/io/github/garrettsummerfi3ld/callout/MainActivity.java
+++ app/src/main/java/io/github/garrettsummerfi3ld/callout/MainActivity.java
@@ -16,10 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
     // Local vars
     private static final int CALLOUT_PERMISSIONS_REQUEST_SEND_SMS = 1;
-    private final String calloutMessage = getString(R.string.callout_message_debug_test);
+    private final String calloutMessage = "This is a test of the Callout system on the users phone, if you have received this, please ignore.";
 
     // Triggered by pressing the callout button in the main activity
     public void calloutMessage(View view) {
+
         // Confirmation toast
         Toast.makeText(this, "[DEBUG] Calling out to your contacts!", Toast.LENGTH_LONG).show();
         Log.i("calloutMessage","Triggered toast message");
