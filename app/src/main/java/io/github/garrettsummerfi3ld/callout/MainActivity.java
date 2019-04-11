8777d5b9724eddb31aee7672391cc36ffe0a1588
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-04-11 00:14:49 -0500

    Manifested two fragments, and attempting more permission reqs
    - Fixed up strings.xml
    - Removed some elements with the activity_main.xml
    - Two fragments created (this should be temporary)
    - calloutMessage now pulls from strings.xml correctly
    - Still working on permission requesting (it will fail to prompt on master branch)

--- app/src/main/java/io/github/garrettsummerfi3ld/callout/MainActivity.java
+++ app/src/main/java/io/github/garrettsummerfi3ld/callout/MainActivity.java
@@ -1,59 +1,130 @@
 package io.github.garrettsummerfi3ld.callout;
 
 import android.Manifest;
-import android.content.Intent;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // Global vars
-    EditText  debugPhoneNumber;
+    private static final int REQUEST_READ_CONTACTS = 79;
     SmsManager smsManager = SmsManager.getDefault();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Pulls values from the textbox in the main activity
-        debugPhoneNumber = findViewById(R.id.debugPhoneNumberInput);
 
-        // Checks in package manager for permissions to  use SMS and contacts
-        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)) {
-            if ((ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS) && (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)))) {
-
+        // On run, there is a check for permissions
+        // Run through Contact permissions first, then check for SMS
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+                getContacts();
             } else {
-                Object CALLOUT_REQUEST_READ_CONTACTS;
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, new int[]);
+                requestSMSPermission();
+            }
+        } else {
+            requestContactPermission();
+        }
+    }
+
+    // Request Reading Contact Permission
+    protected void requestContactPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+            // show UI part if you want here to show some rationale !!!
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS}, REQUEST_READ_CONTACTS);
+        }
+    }
+
+    // Request Sending SMS Permission
+    protected void requestSMSPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.SEND_SMS)) {
+            // show UI part if you want here to show some rationale !!!
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.SEND_SMS}, REQUEST_SEND_SMS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContacts();
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
+
+    public void getContacts() {
+
+        String phoneNumber = null;
+
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        StringBuffer output = new StringBuffer();
+
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor cursor = contentResolver.query(CONTENT_URI, null, null, null, null);
+
+        // Loop for every contact in the phone  
+        if (cursor.getCount() > 0) {
+
+            while (cursor.moveToNext()) {
+                // Local vars
+                String contact_id = cursor.getString(cursor.getColumnIndex(_ID));
+                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
+                if (hasPhoneNumber > 0) {
+                    output.append("\n First Name:" + name);
+                    // Query and loop for every phone number of the contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[]{contact_id}, null);
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        output.append("\n Phone number:" + phoneNumber);
+                    }
+                    phoneCursor.close();
+                }
+                output.append("\n");
             }
         }
     }
 
     // Triggered by pressing the callout button in the main activity
     public void calloutMessage(View view) {
-        // Local vars
-        String calloutMessage = "This is a test of the Callout system on the users phone, if you have received this, please ignore.";
-        String calloutDebugPhoneInput = debugPhoneNumber.getText().toString();
-
         // Try catch with SMS
         try {
             // SMS Handler
-            smsManager.sendTextMessage(calloutDebugPhoneInput, null, calloutMessage, null, null);
+            smsManager.sendTextMessage(String.valueOf(R.string.callout_title), null, String.valueOf(R.string.callout_message_debug), null, null);
             Log.i("calloutMessage", "Send message to number: '" + calloutDebugPhoneInput + "'!");
 
             // Confirmation toast and logging
-            Toast.makeText(this, "[DEBUG] Calling out to your contacts!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Calling out to your contacts!", Toast.LENGTH_LONG).show();
             Log.i("calloutMessage", "Triggered toast message");
         } catch (Exception ex) {
             // Failure toast, logging, and printing stacktrace of the error
-            Toast.makeText(this, "[DEBUG] SMS Failed! Check logcat!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "SMS Failed! Check logcat!", Toast.LENGTH_LONG).show();
             Log.e("calloutMessage", "Error! Printing stacktrace...");
             ex.printStackTrace();
         }
