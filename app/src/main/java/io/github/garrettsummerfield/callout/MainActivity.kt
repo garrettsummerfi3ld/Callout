e9fe753cef893887ab7dc29fffd58961e3577da6
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2019-10-15 00:14:53 -0500

    Starting permissions work

--- app/src/main/java/io/github/garrettsummerfield/callout/MainActivity.kt
+++ app/src/main/java/io/github/garrettsummerfield/callout/MainActivity.kt
@@ -1,12 +1,25 @@
 package io.github.garrettsummerfield.callout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import android.Manifest
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // List of requested permissions
+        val permissionsList = listOf(
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
     }
+
+
 }
